b68e15f91291aa5a4ac8a5fea04373b4f1c7df3c
Luebu <maximilian.luecker@hebk.de>
2020-08-24 23:42:12 +0200

    Clean

--- src/Krieger.java
+++ src/Krieger.java
@@ -1,14 +1,13 @@
 public class Krieger extends Held {
 
 
-    private final int ausdauer = 20;
-
     Krieger(int pStaerke, int pAngriffswert, int pLebenspunkte) {
         super(pStaerke, pAngriffswert, pLebenspunkte);
 
     }
 
     public int angriffswertBerechnen() {
+        int ausdauer = 20;
         return (getWaffe().bonusBerechnen() + getStearke()) + ausdauer;
     }
 }
